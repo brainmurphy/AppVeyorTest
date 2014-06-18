f92cb9472ea0cbd12d7ba90fea30c7ffda80d041
Brian Murphy <brianmurphyyy@gmail.com>
2014-06-18 21:51:37 +0100

    Create the universe.

---
+++ AppVeyorTest/AppVeyorTestWebRole/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppVeyorTestWebRole.Startup))]
+namespace AppVeyorTestWebRole
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
